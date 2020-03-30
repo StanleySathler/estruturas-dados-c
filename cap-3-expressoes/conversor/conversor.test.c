526cadee1e33ea034fafe8722f3454354bb8b6c1
StanleySathler <stanleysathlerpinto@gmail.com>
2020-03-30 15:07:05 -0300

    fix(cap-3): improve conversor implementation to handle more cases

--- cap-3-expressoes/conversor/conversor.test.c
+++ cap-3-expressoes/conversor/conversor.test.c
@@ -2,29 +2,32 @@
 #include "include/assert.h"
 #include "conversor.h"
 
-/**
- * Clear the string. Must be called before each test.
- */
-#define PREPARE(actual) memset(actual, '\0', sizeof(actual))
+#define MAX 100
+#define AFTER_EACH(actual) memset(actual, '\0', MAX)
+
+void assert(const char expected[], char actual[], const char message[])
+{
+  assert_string(expected, actual, message);
+  AFTER_EACH(actual);
+}
 
 int
 main()
 {
-  char actual[100];
+  char actual[MAX];
 
-  PREPARE(actual);
   postfix_convert("2 * 3 + 8 / 4", actual);
-  assert_string(
-    "23*84/+",
-    actual,
-    "[2 * 3 + 8 / 4]"
-  );
+  assert("23*84/+", actual, "[2 * 3 + 8 / 4]");
 
-  PREPARE(actual);
   postfix_convert("(2 * 5) + (8 + 3)", actual);
-  assert_string(
-    "25*83++",
-    actual,
-    "[(2 * 5) + (8 + 3)]"
-  );
+  assert("25*83++", actual, "[(2 * 5) + (8 + 3)]");
+
+  postfix_convert("2 * 5 + 3 / 2 + 1", actual);
+  assert("25*32/+1+", actual, "[2 * 5 + 3 / 2 + 1]");
+
+  postfix_convert("2 * (3 + 1 * 2)", actual);
+  assert("2312*+*", actual, "[2 * (3 + 1 * 2)]");
+
+  postfix_convert("2 * (3 * 2 + 4 * 7)", actual);
+  assert("232*47*+*", actual, "[2 * (3 * 2 + 4 * 7)]");
 }
